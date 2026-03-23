d68ed680e38a84de675a6bf86be25b1d37d9ad52
louverture-t <d2kole@proton.me>
2026-03-22 22:10:31 -0400

    fix: move React hooks and ApolloProvider to @apollo/client/react (v4 compat)

--- Starter/frontend/src/App.jsx
+++ Starter/frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
-import { gql, useQuery, useMutation, useApolloClient } from '@apollo/client';
+import { gql } from '@apollo/client';
+import { useQuery, useMutation, useApolloClient } from '@apollo/client/react';
 
 const GET_QUESTIONS = gql`
   query Questions {
